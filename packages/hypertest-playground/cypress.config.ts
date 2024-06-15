e52291c1080a37fc5e6abbec358559e2a7007546
Blaze <blaze.blaut@gmail.com>
2024-06-15 12:32:03 +0200

    Remove cypress config overrides

--- packages/hypertest-playground/cypress.config.ts
+++ packages/hypertest-playground/cypress.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from "cypress";
 
-export default defineConfig({
+const config = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on('task', {
-        log(message) {
-          console.log(message)
-
-          return null
-        },
-      })
-    },
+    setupNodeEvents(on, config) {}
   },
 });
+
+
+export default config
