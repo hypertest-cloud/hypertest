444e07058a9b4af53a8ba8587bb6ab136652aea2
Blaze <blaze.blaut@gmail.com>
2024-06-15 11:27:16 +0200

    Add it callback skip example

--- packages/hypertest-playground/cypress/e2e/1-getting-started/todo.cy.js
+++ packages/hypertest-playground/cypress/e2e/1-getting-started/todo.cy.js
@@ -28,7 +28,7 @@ describe('example to-do app', () => {
     cy.visit('https://example.cypress.io/todo')
   })
 
-  it('displays two todo items by default', () => {
+  it.skip('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
