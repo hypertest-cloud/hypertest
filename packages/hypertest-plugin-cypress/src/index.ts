a3698cba3376be928a4a7ad8990f932349e91b67
Blaze <blaze.blaut@gmail.com>
2024-06-15 12:37:00 +0200

    Add override and restoring base tests

--- packages/hypertest-plugin-cypress/src/index.ts
+++ packages/hypertest-plugin-cypress/src/index.ts
@@ -8,11 +8,12 @@ import { CypressPluginOptions } from "./types.js";
 
 export const Plugin = (options: CypressPluginOptions): HypertestPlugin => ({
   getTestCount: async () => new Promise(async (resolve, reject) => {
+    await overrideItCallback(options)
     const execOptions = {
       cwd: options.projectPath
     }
 
-    exec('npx cypress run --headless', execOptions, (error, stdout, stderr) => {
+    exec('npx cypress run --headless', execOptions, async (error, stdout, stderr) => {
       if (error) {
         const errorMessage = `Error executing: ${error}`
 
@@ -23,6 +24,7 @@ export const Plugin = (options: CypressPluginOptions): HypertestPlugin => ({
       const testsCount = stdout.split(TEST_COUNTER_MARK).length - 1
       const skippedTestsCount = stdout.split(SKIPPED_TEST_COUNTER_MARK).length - 1
 
+      await removeItCallbackOverride(options)
       resolve(testsCount + skippedTestsCount)
     })
   }),
