d347f1c7675b025b680d18118a4fd445f475c889
Blaze <blaze.blaut@gmail.com>
2024-06-15 11:28:56 +0200

    Implement plugin get length first glanc

--- packages/hypertest-plugin-cypress/src/index.ts
+++ packages/hypertest-plugin-cypress/src/index.ts
@@ -2,25 +2,28 @@ import {
   HypertestPlugin,
 } from "@hypertest/hypertest-core";
 import { exec } from 'child_process';
-import cypress from 'cypress'
+import { overrideItCallback, removeItCallbackOverride } from "./replace-it-callback.js";
+import { SKIPPED_TEST_COUNTER_MARK, TEST_COUNTER_MARK } from "./constant.js";
+import { CypressPluginOptions } from "./types.js";
 
-export const Plugin = (options: {}): HypertestPlugin => ({
-  getTestCount: async () => {
-    const dd = exec('npx cypress run --headless', {
-      cwd: 'C://Praca//hypertest//packages//hypertest-playground'
-    }, (error, stdout, stderr) => {
-      console.log('asd')
+export const Plugin = (options: CypressPluginOptions): HypertestPlugin => ({
+  getTestCount: async () => new Promise(async (resolve, reject) => {
+    const execOptions = {
+      cwd: options.projectPath
+    }
+
+    exec('npx cypress run --headless', execOptions, (error, stdout, stderr) => {
       if (error) {
-        console.error(`Error executing ${error}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Error output from`, stderr);
-        return;
-      }
-      console.log(`Output from: ${stdout}`);
-    })
+        const errorMessage = `Error executing: ${error}`
 
-    return 0;
-  },
+        console.error(errorMessage);
+        reject(errorMessage);
+      }
+
+      const testsCount = stdout.split(TEST_COUNTER_MARK).length - 1
+      const skippedTestsCount = stdout.split(SKIPPED_TEST_COUNTER_MARK).length - 1
+
+      resolve(testsCount + skippedTestsCount)
+    })
+  }),
 });
