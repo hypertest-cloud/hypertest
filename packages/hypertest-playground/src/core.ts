ca16bb18d615b47ef075ff6db85579587a64acd6
Blaze <blaze.blaut@gmail.com>
2024-06-15 11:27:59 +0200

    Adjust core and runner to new types

--- packages/hypertest-playground/src/core.ts
+++ packages/hypertest-playground/src/core.ts
@@ -2,7 +2,7 @@ import { HypertestCore } from "@hypertest/hypertest-core";
 import { Plugin as CypressPlugin } from "@hypertest/hypertest-plugin-cypress";
 
 const plugin = CypressPlugin({
-  path: 'C://Praca//hypertest//packages//hypertest-playground'
+  projectPath: 'C://Praca//hypertest//packages//hypertest-playground'
 });
 
 const hypertest = HypertestCore({
--- packages/hypertest-playground/src/runner.ts
+++ packages/hypertest-playground/src/runner.ts
@@ -2,11 +2,11 @@ import { HypertestCore } from "@hypertest/hypertest-core";
 import { Plugin } from "@hypertest/hypertest-plugin-cypress";
 
 const plugin = Plugin({
-  path: 'C://Praca//hypertest//packages//hypertest-playground'
+  projectPath: 'C://Praca//hypertest//packages//hypertest-playground'
 });
 
 const hypertest = HypertestCore({
   plugin,
 });
 
-hypertest.run().then(console.log);
+hypertest.run();
