01aa0bf3c307e395f4a47b8bb311fc655da625a0
Blaze <blaze.blaut@gmail.com>
2024-07-20 17:45:29 +0200

    Add TEST_PROJECT_PATH env variable

--- packages/hypertest-playground/src/runner.ts
+++ packages/hypertest-playground/src/runner.ts
@@ -1,8 +1,13 @@
 import { HypertestCore } from "@hypertest/hypertest-core";
 import { Plugin } from "@hypertest/hypertest-plugin-cypress";
 
+const projectPath = process.env.TEST_PROJECT_PATH
+if (!projectPath) {
+  throw new Error('Variable TEST_PROJECT_PATH is missing.')
+}
+
 const plugin = Plugin({
-  projectPath: 'C://Praca//hypertest//packages//hypertest-playground'
+  projectPath
 });
 
 const hypertest = HypertestCore({
