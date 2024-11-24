31dfadd0480ac524d1122c5c6240f4b98b2f8af5
Blaze <blaze.blaut@gmail.com>
2024-11-24 12:12:01 +0100

    Add getFileTestNames util

---
+++ packages/hypertest-plugin-playwright/src/getFileTestNames.ts
@@ -0,0 +1,51 @@
+import { TestDescription } from '@hypertest/hypertest-core';
+import * as fs from 'fs';
+import * as ts from "typescript";
+
+// TODO use uuid
+const SEPARATOR = 'AJWFBasdw'
+
+const FILE_HEADER = `
+  const currentDescription = []
+
+  const test = (name) => {
+    if (currentDescription.length > 0) {
+      console.log(currentDescription.join('${SEPARATOR}') + '${SEPARATOR}' + name);
+    } else {
+     console.log(name);
+    }
+  }
+
+  test.beforeEach = () => {}
+  test.afterEach = () => {}
+  test.describe = (name, callback) => {
+    currentDescription.push(name)
+    callback()
+    currentDescription.pop()
+  }
+`
+
+export const getFileTestNames = async (filePath: string): Promise<Pick<TestDescription, 'testName' | 'contextPath'>[]> => {
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+    .replace('import', '// import')
+
+  let result = ts.transpileModule(FILE_HEADER + fileContent, {});
+
+  const names: Pick<TestDescription, 'testName' | 'contextPath'>[] = []
+
+  const originalConsoleLog = console.log;
+  console.log = (message) => {
+    const chain = message.split(SEPARATOR)
+    const testName = chain.pop()
+    names.push({
+      contextPath: chain.join('/'),
+      testName,
+    })
+  };
+
+  let runnalbe:any = eval(result.outputText);
+
+  console.log = originalConsoleLog;
+
+  return names
+}
--- packages/hypertest-plugin-playwright/src/index.ts
+++ packages/hypertest-plugin-playwright/src/index.ts
@@ -1,35 +1,28 @@
 import {
   HypertestPlugin,
+  TestDescription,
 } from "@hypertest/hypertest-core";
-import * as fs from 'fs';
-import * as path from 'path';
 import { PlaywrightPluginOptions } from "./types.js";
+import { getFileTestNames } from "./getFileTestNames.js";
+import { getSpecFiles } from "./getSpecFiles.js";
 
 const PLAYWRIGHT_DIRECTORY = 'tests'
 
-function getSpecFiles(dir: string): string[] {
-  let results: string[] = [];
-  const files = fs.readdirSync(dir);
-
-  for (const file of files) {
-    const fullPath = path.join(dir, file);
-    const stat = fs.statSync(fullPath);
-
-    if (stat.isDirectory()) {
-      results = results.concat(getSpecFiles(fullPath));
-    } else if (file.endsWith('.spec.ts')) {
-      results.push(fullPath);
-    }
-  }
-
-  return results;
-}
-
 export const Plugin = (options: PlaywrightPluginOptions): HypertestPlugin => ({
-  getTestDescription: async () => new Promise(async (resolve, reject) => {
+  getTestDescriptions: async () => new Promise<TestDescription[]>(async (resolve, reject) => {
     const specFiles = getSpecFiles(options.projectPath + '/' + PLAYWRIGHT_DIRECTORY);
-    console.log('specFiles:', fs.readFileSync(specFiles[0], 'utf8'))
+    console.log('specFiles: ', specFiles)
+    const result = await Promise.all(specFiles.map(async (specFile) => {
+      const names = await getFileTestNames(specFile)
 
-    resolve([])
+      return names.map((name) => ({
+        directoryPath: specFile,
+        contextPath: name.contextPath,
+        testName: name.testName,
+      }))
+    }))
+
+
+    resolve(result.flat())
   }),
 });
