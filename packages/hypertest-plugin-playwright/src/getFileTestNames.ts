bcc3f44e06f02d808020f1cc387166bdb1f26099
Szymon Janota <17457793+szymonjanota@users.noreply.github.com>
2024-11-27 17:21:51 +0100

    Add proxy

--- packages/hypertest-plugin-playwright/src/getFileTestNames.ts
+++ packages/hypertest-plugin-playwright/src/getFileTestNames.ts
@@ -1,9 +1,9 @@
 import { TestDescription } from '@hypertest/hypertest-core';
 import * as fs from 'fs';
-import * as ts from "typescript";
-import { v1 } from 'uuid'
+import * as ts from 'typescript';
+import { v1 } from 'uuid';
 
-const SEPARATOR = v1()
+const SEPARATOR = v1();
 
 const FILE_HEADER = `
   const currentDescription = []
@@ -23,29 +23,87 @@ const FILE_HEADER = `
     callback()
     currentDescription.pop()
   }
-`
+`;
 
-export const getFileTestNames = async (filePath: string): Promise<Pick<TestDescription, 'testName' | 'contextPath'>[]> => {
-  const fileContent = fs.readFileSync(filePath, 'utf8')
-    .replace('import', '// import')
+const globalFunctionProxyFactory = (
+  names: Pick<TestDescription, 'testName' | 'contextPath'>[],
+) => {
+  const proxy = new Proxy(() => {}, {
+    apply: () => {
+      return;
+    },
+    get: (target, prop) => {
+      if (Symbol.unscopables === prop) {
+        return undefined;
+      }
+      if (prop === 'eval') {
+        // biome-ignore lint/security/noGlobalEval: <explanation>
+        return eval;
+      }
+      if (prop === 'console') {
+        return console;
+      }
+      if (prop === 'test') {
+        console.log('test2');
+        const fn = (name: string) => {
+          console.log(name);
+          return name;
+        };
+        return new Proxy(fn, {
+          get: (target, prop) => {
+            if (Symbol.unscopables === prop) {
+              return undefined;
+            }
+            if (prop === 'describe') {
+              return;
+            }
+            return proxy;
+          },
+        });
+      }
+
+      console.log('get', prop);
+
+      return proxy;
+    },
+    has: () => {
+      return true;
+    },
+  });
+
+  return proxy;
+};
+
+export const getFileTestNames = async (
+  filePath: string,
+): Promise<Pick<TestDescription, 'testName' | 'contextPath'>[]> => {
+  const fileContent = fs
+    .readFileSync(filePath, 'utf8')
+    .replace('import', '// import');
 
   let result = ts.transpileModule(FILE_HEADER + fileContent, {});
 
-  const names: Pick<TestDescription, 'testName' | 'contextPath'>[] = []
+  const names: Pick<TestDescription, 'testName' | 'contextPath'>[] = [];
+  const globalFunctionProxy = globalFunctionProxyFactory(names);
 
   const originalConsoleLog = console.log;
   console.log = (message) => {
-    const chain = message.split(SEPARATOR)
-    const testName = chain.pop()
+    const chain = message.split(SEPARATOR);
+    const testName = chain.pop();
     names.push({
       contextPath: chain.join('/'),
       testName,
-    })
+    });
   };
 
-  let runnalbe:any = eval(result.outputText);
+  new Function(
+    'with (this.globalFunctionProxy) { eval(this.outputText) }',
+  ).call({
+    globalFunctionProxy,
+    outputText: result.outputText,
+  });
 
   console.log = originalConsoleLog;
 
-  return names
-}
+  return names;
+};
