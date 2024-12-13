f171685f029f8a2d9aa7c492ca522a3e10f35b0e
Marcin Łesek <daav95@gmail.com>
2024-12-13 12:30:49 +0100

    feat: add base lambda invoke function

--- packages/hypertest-provider-cloud-aws/src/index.ts
+++ packages/hypertest-provider-cloud-aws/src/index.ts
@@ -1,11 +1,33 @@
+import { Lambda } from "@aws-sdk/client-lambda";
+import { fromEnv } from "@aws-sdk/credential-providers";
 import { HypertestProviderCloud } from "@hypertest/hypertest-core";
+import { lambda } from "./lambda.js";
 
 interface HypertestProviderCloudAWS extends HypertestProviderCloud { }
 
 interface HypertestProviderCloudAWSSettings {}
 
-export const HypertestProviderCloudAWS = (settings: HypertestProviderCloudAWSSettings): HypertestProviderCloudAWS => ({
-  setImage: async () => {},
-  spawn: async () => {},
-  getStatus: async (id: string) => {},
-})
+const AWS_REGION = 'us-east-1';
+const FUNC_NAME = 'my_func_name'
+const PAYLOAD = 'console.log("hello world")'
+
+export const HypertestProviderCloudAWS = (settings: HypertestProviderCloudAWSSettings): HypertestProviderCloudAWS => {
+  const lambdaClient = new Lambda({
+    credentials: fromEnv(),
+    region: AWS_REGION,
+  });
+
+  return {
+    setImage: async () => {},
+    spawn: async () => {
+      console.log(lambda.create({
+        client: lambdaClient,
+        args: {
+          FunctionName: FUNC_NAME,
+          Payload: PAYLOAD,
+        }
+      }))
+    },
+    getStatus: async (id: string) => {},
+  }
+}
