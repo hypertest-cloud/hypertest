288eadd8a94cd7e73cb5ef037db6369326d3eaa5
Blaze <blaze.blaut@gmail.com>
2024-12-16 17:36:19 +0100

    Implement spawn util scaffold

--- packages/hypertest-provider-cloud-aws/src/index.ts
+++ packages/hypertest-provider-cloud-aws/src/index.ts
@@ -1,4 +1,4 @@
-import { Lambda } from "@aws-sdk/client-lambda";
+import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
 import { fromEnv } from "@aws-sdk/credential-providers";
 import { HypertestProviderCloud } from "@hypertest/hypertest-core";
 import { lambda } from "./lambda.js";
@@ -12,7 +12,7 @@ const FUNC_NAME = 'my_func_name'
 const PAYLOAD = 'console.log("hello world")'
 
 export const HypertestProviderCloudAWS = (settings: HypertestProviderCloudAWSSettings): HypertestProviderCloudAWS => {
-  const lambdaClient = new Lambda({
+  const lambdaClient = new LambdaClient({
     credentials: fromEnv(),
     region: AWS_REGION,
   });
@@ -20,13 +20,21 @@ export const HypertestProviderCloudAWS = (settings: HypertestProviderCloudAWSSet
   return {
     setImage: async () => {},
     spawn: async () => {
-      console.log(lambda.create({
-        client: lambdaClient,
-        args: {
-          FunctionName: FUNC_NAME,
-          Payload: PAYLOAD,
-        }
-      }))
+      const command = new InvokeCommand({
+        FunctionName: FUNC_NAME,
+        Payload: PAYLOAD,
+      });
+      const { Payload, LogResult } = await lambdaClient.send(command);
+
+      if (!Payload || !LogResult) {
+        throw new Error('Lambda Payload or LogResult is missing.');
+      }
+
+      const logs = Buffer.from(LogResult, "base64").toString();
+      const result = Buffer.from(Payload).toString();
+
+      console.log('lambda spawn logs: ', logs)
+      console.log('lambda result: ', result)
     },
     getStatus: async (id: string) => {},
   }
