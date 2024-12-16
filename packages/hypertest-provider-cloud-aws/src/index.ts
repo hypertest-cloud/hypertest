fd216b59f75a315a736332e79f64455bca312988
Blaze <blaze.blaut@gmail.com>
2024-12-16 17:54:47 +0100

    Install aws-sdk/client-ecr and spawn erc client

--- packages/hypertest-provider-cloud-aws/src/index.ts
+++ packages/hypertest-provider-cloud-aws/src/index.ts
@@ -1,4 +1,5 @@
 import { InvokeCommand, LambdaClient } from "@aws-sdk/client-lambda";
+import { ECRClient } from "@aws-sdk/client-ecr";
 import { fromEnv } from "@aws-sdk/credential-providers";
 import { HypertestProviderCloud } from "@hypertest/hypertest-core";
 import { lambda } from "./lambda.js";
@@ -13,6 +14,10 @@ const PAYLOAD = 'console.log("hello world")'
 
 export const HypertestProviderCloudAWS = (settings: HypertestProviderCloudAWSSettings): HypertestProviderCloudAWS => {
   const lambdaClient = new LambdaClient({
+    credentials: fromEnv(),
+    region: AWS_REGION,
+  });
+  const ecrClient = new ECRClient({
     credentials: fromEnv(),
     region: AWS_REGION,
   });
