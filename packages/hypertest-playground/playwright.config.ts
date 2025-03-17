91f42089c4c544ef0d37c81a9956f0a6d07bcee8
Blaze <blaze.blaut@gmail.com>
2025-03-17 15:46:11 +0100

    Prepare playground playwright config for lambda usage (hardcoded)

--- packages/hypertest-playground/playwright.config.ts
+++ packages/hypertest-playground/playwright.config.ts
@@ -12,73 +12,94 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './playwright/tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // TODO: Verify how we can set this variable in Lambda
-  // This handles `playwright-results` directory
-  reporter: [['json', { outputFile: '/tmp/playwright-results.json' }]],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+  // testDir: './playwright/tests',
+  // /* Run tests in files in parallel */
+  // fullyParallel: true,
+  // /* Fail the build on CI if you accidentally left test.only in the source code. */
+  // forbidOnly: !!process.env.CI,
+  // /* Retry on CI only */
+  // retries: process.env.CI ? 2 : 0,
+  // /* Opt out of parallel tests on CI. */
+  // workers: process.env.CI ? 1 : undefined,
+  // /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  // // TODO: Verify how we can set this variable in Lambda
+  // // This handles `playwright-results` directory
+  // reporter: [['json', { outputFile: '/tmp/playwright-results.json' }]],
+  // /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  // use: {
+  //   /* Base URL to use in actions like `await page.goto('/')`. */
+  //   // baseURL: 'http://127.0.0.1:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: 'on-first-retry',
+  // },
+  // // TODO: Verify how we can set this variable in Lambda
+  // // This handles `test-results` directory
+  // outputDir: '/tmp/playwright-results',
+
+  // /* Configure projects for major browsers */
+  // projects: [
+  //   {
+  //     name: 'chromium',
+  //     use: { ...devices['Desktop Chrome'] },
+  //   },
+
+  //   // {
+  //   //   name: 'firefox',
+  //   //   use: { ...devices['Desktop Firefox'] },
+  //   // },
+
+  //   // {
+  //   //   name: 'webkit',
+  //   //   use: { ...devices['Desktop Safari'] },
+  //   // },
+
+  //   /* Test against mobile viewports. */
+  //   // {
+  //   //   name: 'Mobile Chrome',
+  //   //   use: { ...devices['Pixel 5'] },
+  //   // },
+  //   // {
+  //   //   name: 'Mobile Safari',
+  //   //   use: { ...devices['iPhone 12'] },
+  //   // },
+
+  //   /* Test against branded browsers. */
+  //   // {
+  //   //   name: 'Microsoft Edge',
+  //   //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+  //   // },
+  //   // {
+  //   //   name: 'Google Chrome',
+  //   //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+  //   // },
+  // ],
+
+  // /* Run your local dev server before starting the tests */
+  // // webServer: {
+  // //   command: 'npm run start',
+  // //   url: 'http://127.0.0.1:3000',
+  // //   reuseExistingServer: !process.env.CI,
+  // // },
+
+  testDir: '/function/playwright/tests',
+  fullyParallel: false,
+  forbidOnly: true,
+  retries: 0,
+  workers: 1,
+  reporter: [['json', { outputFile: '/tmp/playwright-results.json' }]],
+  use: {
     trace: 'on-first-retry',
   },
-  // TODO: Verify how we can set this variable in Lambda
-  // This handles `test-results` directory
   outputDir: '/tmp/playwright-results',
-
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chromium',
+        headless: true,
+      },
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
