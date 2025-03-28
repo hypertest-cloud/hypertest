af1c25470276d65f24b6c1db6da2707f5080a3d3
Szymon Janota <17457793+szymonjanota@users.noreply.github.com>
2025-03-28 19:02:56 +0100

    Test grep

--- packages/hypertest-runner-playwright/src/bin.ts
+++ packages/hypertest-runner-playwright/src/bin.ts
@@ -2,4 +2,11 @@ import type { Context } from 'aws-lambda';
 import { handler } from './index.js';
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-handler({} as any, {} as Context);
+handler(
+  {
+    grep:
+      process.env.GREP ??
+      '^chromium\\splaywright/tests/demo-todo-app\\.spec\\.ts\\sdesc\\stest2$',
+  } as any,
+  {} as Context,
+);
