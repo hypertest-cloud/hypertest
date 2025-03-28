0b68e4d3babd64e31b53a81c0c4e37a98ca48d28
Szymon Janota <17457793+szymonjanota@users.noreply.github.com>
2025-03-29 00:17:53 +0100

    Remove cypress

--- packages/hypertest-playground/cypress/cypress.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    waitRandom(min: number, max: number): void;
-  }
-}
--- packages/hypertest-plugin-cypress/src/types.ts
+++
@@ -1,3 +0,0 @@
-export interface CypressPluginOptions {
-  projectPath: string
-}
