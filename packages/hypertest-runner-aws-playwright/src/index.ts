4097d6040408fe61cae7ac32c1dc6f057cb5b15a
Marcin Łesek <daav95@gmail.com>
2025-07-31 12:17:06 +0200

    fix: verify ffmpeg symlink existence before creating it

--- packages/hypertest-runner-aws-playwright/src/index.ts
+++ packages/hypertest-runner-aws-playwright/src/index.ts
@@ -3,6 +3,7 @@ process.env.HOME = '/tmp';
 import chromium from '@sparticuz/chromium';
 import type { Context } from 'aws-lambda';
 import { execSync } from 'node:child_process';
+import { existsSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import { uploadToS3 } from './utils/uploadToS3.js';
 
@@ -38,9 +39,18 @@ console.log(userConfig);
 export default userConfig;
 `;
 
-async function main(runId: string, testId: string, bucketName: string, grep?: string) {
+async function main(
+  runId: string,
+  testId: string,
+  bucketName: string,
+  grep?: string,
+) {
   const testRunDir = `/tmp/${runId}/${testId}`;
   const testOutputDir = `${testRunDir}/output`;
+
+  // ffmpeg
+  const ffmpegSourcePath = '/usr/bin/ffmpeg';
+  const ffmpegTargetPath = '/tmp/.cache/ms-playwright/ffmpeg-1011/ffmpeg-linux';
 
   await fs.mkdir(testOutputDir, { recursive: true });
 
@@ -60,11 +70,7 @@ async function main(runId: string, testId: string, bucketName: string, grep?: st
     ? `HT_TEST_ARTIFACTS_OUTPUT_PATH=${testOutputDir} npx playwright test -c ${testRunDir}/_playwright.config.ts --grep "${grep}"`
     : `HT_TEST_ARTIFACTS_OUTPUT_PATH=${testOutputDir} npx playwright test -c ${testRunDir}/_playwright.config.ts`;
 
-  // Ensure the ffmpeg cache directory exists and create a symlink
-  // to the system ffmpeg binary. This is necessary for Playwright
-  // to handle video recording. The directory structure is based on
-  // Playwright's architecture. The symlink points to the system's
-  // ffmpeg binary.
+  // Ensure the ffmpeg cache directory exists.
   try {
     execSync('mkdir -p /tmp/.cache/ms-playwright/ffmpeg-1011', {
       stdio: 'inherit',
@@ -75,17 +81,29 @@ async function main(runId: string, testId: string, bucketName: string, grep?: st
     console.log('Error creating directory for ffmpeg cache:', error);
   }
 
+  // Check if ffmpeg symlink exists, if not create a symlink
+  // to the system ffmpeg binary. This is necessary for Playwright
+  // to handle video recording. The directory structure is based on
+  // Playwright's architecture. The symlink points to the system's
+  // ffmpeg binary.
   try {
-    execSync(
-      'ln -s /usr/bin/ffmpeg /tmp/.cache/ms-playwright/ffmpeg-1011/ffmpeg-linux',
-      {
+    if (existsSync(ffmpegTargetPath)) {
+      // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+      console.log(`ffmpeg symlink already exists: ${ffmpegTargetPath}`);
+    } else {
+      execSync(`ln -s ${ffmpegSourcePath} ${ffmpegTargetPath}`, {
         stdio: 'inherit',
         cwd: process.cwd(),
-      },
-    );
+      });
+
+      // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+      console.log(
+        `ffmpeg symlink created: ${ffmpegTargetPath} -> ${ffmpegSourcePath}`,
+      );
+    }
   } catch (error) {
     // biome-ignore lint/suspicious/noConsoleLog: <explanation>
-    console.log('Error creating symlink for ffmpeg:', error);
+    console.log('Error creating ffmpeg symlink:', error);
   }
 
   // Run the Playwright test command.
@@ -99,7 +117,12 @@ async function main(runId: string, testId: string, bucketName: string, grep?: st
     console.log('main test run error:', error);
   }
 
-  const uploadResult = await uploadToS3(bucketName, testOutputDir, runId, testId);
+  const uploadResult = await uploadToS3(
+    bucketName,
+    testOutputDir,
+    runId,
+    testId,
+  );
   if (!uploadResult.success) {
     throw new Error('Failed to upload test results to S3.');
   }
@@ -118,14 +141,24 @@ async function main(runId: string, testId: string, bucketName: string, grep?: st
 }
 
 const handler = async (
-  event: { runId: string; testId: string; bucketName: string; context: EventContext },
+  event: {
+    runId: string;
+    testId: string;
+    bucketName: string;
+    context: EventContext;
+  },
   context: Context,
 ) => {
   // biome-ignore lint/suspicious/noConsoleLog: <explanation>
   console.log(event, context);
 
   try {
-    return await main(event.runId, event.testId, event.bucketName, event.context?.grep);
+    return await main(
+      event.runId,
+      event.testId,
+      event.bucketName,
+      event.context?.grep,
+    );
   } catch (err) {
     console.error(err);
 
