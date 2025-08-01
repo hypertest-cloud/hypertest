42d482ab3918454cf7635eefc1122b1768bae411
Marcin Łesek <daav95@gmail.com>
2025-08-01 13:58:21 +0200

    test: dummy change in runner-aws-playwright package

--- packages/hypertest-runner-aws-playwright/src/index.ts
+++ packages/hypertest-runner-aws-playwright/src/index.ts
@@ -7,7 +7,7 @@ import { existsSync } from 'node:fs';
 import fs from 'node:fs/promises';
 import { uploadToS3 } from './utils/uploadToS3.js';
 
-// TODO: remove that comment.
+// TODO: remove that comment
 
 interface EventContext {
   grep: string;
