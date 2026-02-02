c78f7af749447388ea683cd2151a3a8154446f71
Blaze <blaze.blaut@gmail.com>
2026-02-02 17:42:25 +0100

    Update unix grep combining OS agnostic

--- packages/hypertest-plugin-playwright/src/getGrepString.ts
+++ packages/hypertest-plugin-playwright/src/getGrepString.ts
@@ -1,19 +1,30 @@
 import path from 'node:path';
 import escapeStringRegexp from 'escape-string-regexp';
 
+const getRelativeUnixPath = (from: string, to: string): string => {
+  return path.relative(from, to).split(path.sep).join('/');
+};
+
+const escapeForTerminalRegex = (value: string): string => {
+  const regexSafe = escapeStringRegexp(value);
+
+  return regexSafe.replace(/\\/g, '\\\\');
+};
+
 export const getGrepString = (
   projectName: string,
   testDirectory: string,
   specFilePath: string,
   testContextPath: string,
 ) => {
-  const combinedStr = [
+  const rawParts = [
     projectName,
-    path.relative(testDirectory, specFilePath),
+    getRelativeUnixPath(testDirectory, specFilePath),
     testContextPath,
-  ]
-    .map(escapeStringRegexp)
-    .join(' ');
+  ];
 
-  return `^${combinedStr}$`;
+  const terminalSafeParts = rawParts.map(escapeForTerminalRegex);
+  const pattern = `^${terminalSafeParts.join(' ')}$`;
+
+  return pattern;
 };
